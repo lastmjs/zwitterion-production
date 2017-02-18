ff146561f66d93ab37e36cc26a52832c87617a10
Jordan Last <jordan.michael.last@gmail.com>
2017-02-18 01:27:19 -0700

    release v0.0.3

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const Builder = require('systemjs-builder');
 const chokidar = require('chokidar');
 
 program
-    .version('0.0.2')
+    .version('0.0.3')
     .option('-p, --port [port]', 'Specify the server\'s port')
     .option('-l, --logs', 'Turn on logging to files in current directory')
     .option('-r, --spa-root [spaRoot]', 'The file to redirect to when a requested file is not found')
