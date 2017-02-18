8798711e3bc0509c11e48c700dae63d14ff05e02
Jordan Last <jordan.michael.last@gmail.com>
2017-02-17 20:08:34 -0700

    get processes to work correctly, add in attention to side effects

--- main.js
+++ main.js
@@ -1,32 +1,59 @@
+// start side-causes, read from the world
 const fs = require('fs');
 const program = require('commander');
+const http = require('http');
+const execSync = require('child_process').execSync;
+const nodeCleanup = require('node-cleanup');
 
 program
     .version('0.0.0')
     .option('-p, --port [port]', 'Specify the server\'s port')
     .parse(process.argv);
+// end side-causes
 
-const port = program.port || 5000;
+// start pure operations, generate the data
+const nginxPort = +(program.port || 5000);
+const nodePort = nginxPort + 1;
+const nginxConf = createNGINXConfigFile(fs, nginxPort, nodePort);
+const httpServer = createNodeServer(http, nodePort);
+//end pure operations
 
-createNGINXConfigFile(port);
+// start side-effects, change the world
+fs.writeFileSync('nginx.conf', nginxConf);
+execSync(`sudo nginx -p . -c nginx.conf && exit 0`);
+console.log(`NGINX listening on port ${nginxPort}`);
+nodeCleanup((exitCode, signal) => {
+    execSync(`sudo nginx -p . -s stop`);
+});
+httpServer.listen(nodePort + 1);
+// end side-effects
 
-function createNGINXConfigFile(port) {
-    fs.writeFileSync('nginx.conf', `
+function createNGINXConfigFile(fs, nginxPort, nodePort) {
+    return `
         events {}
 
         http {
 
             server {
-                listen ${port};
+                listen ${nginxPort};
 
                 access_log http.access.log;
                 error_log http.error.log;
 
-                root .;
                 location / {
+                    proxy_pass http://localhost:${nodePort};
+                }
 
+                location /LICENSE {
+                    proxy_pass http://localhost:${nodePort};
                 }
             }
         }
-    `);
+    `;
+}
+
+function createNodeServer(http, nodePort) {
+    return http.createServer((req, res) => {
+        res.end('hello there sir');
+    });
 }
